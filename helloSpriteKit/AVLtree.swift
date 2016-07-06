d393fc1a0172c407097bab9fbed225a3ca102785
Prachi Bodas <prbodas@fb.com>
2016-07-06 14:29:28 -0700

    tilting works

---
+++ helloSpriteKit/AVLtree.swift
@@ -0,0 +1,146 @@
+//
+//  AVLtree.swift
+//  helloSpriteKit
+//
+//  Created by Prachi Bodas on 7/5/16.
+//  Copyright © 2016 Prachi Bodas. All rights reserved.
+//
+
+import Foundation
+
+
+class AVLtree:NSObject
+{
+    
+    let MAX_HEIGHT = 5
+    
+    var isEmpty: Bool = true
+    var node: Int!
+    var left: AVLtree!
+    var right: AVLtree!
+    
+    override init() {
+        super.init()
+        isEmpty = true
+    }
+    
+    func withParams(n:Int, l:AVLtree, r:AVLtree)
+    {
+        isEmpty = false
+        left = l
+        right = r
+        node = n
+    }
+    
+    func getHeight() -> Int
+    {
+        if (self.isEmpty)
+        {
+            return 0
+        }else{
+            let lheight = left.getHeight()
+            let rheight = right.getHeight()
+            if (lheight>rheight)
+            {
+                return (1+lheight)
+            }else{
+                return (1+rheight)
+            }
+        }
+    }
+    
+    func balanceFactor() -> Int
+    {
+        if (self.isEmpty)
+        {
+            return 0
+        }else{
+            return left.getHeight() - right.getHeight()
+        }
+    }
+    
+    //duplicates ignored
+    func insertIntNoRotation(n:Int)
+    {
+        if (isEmpty)
+        {
+            isEmpty = false
+            node = n
+            left = AVLtree.init()
+            right = AVLtree.init()
+        }else{
+            if (n > node)
+            {
+                print("right insert \(n)")
+                right.insertIntNoRotation(n)
+            }else if (n < node)
+            {
+                print("left insert \(n)")
+                left.insertIntNoRotation(n)
+            }
+        }
+    }
+    
+    //depth starts at 1
+    func getNumsAtDepth(d:Int) -> [Int]
+    {
+        if (isEmpty)
+        {
+            var arr: [Int] = []
+            for _ in 0 ..< Int(pow(2.0, Float(d-1)))
+            {
+                arr.append(-1)
+            }
+            return arr
+        }else if (d == 1)
+        {
+            return [node]
+        }else{
+            return left.getNumsAtDepth(d-1) + right.getNumsAtDepth(d-1)
+        }
+    }
+    
+    
+    //tree rotation code
+    func rotate_left() -> AVLtree
+    {
+        if (!(self.isEmpty || self.right.isEmpty))
+        {
+            let temp = self.right
+            self.right = self.right.left;
+            temp.left = self;
+            return temp
+        }else{
+            print("return empty")
+            return AVLtree.init()
+        }
+    }
+    
+    func rotate_right() -> AVLtree
+    {
+        if (!(self.isEmpty || self.left.isEmpty))
+        {
+            let temp = self.left;
+            self.left = self.left.right;
+            temp.right = self;
+            return temp
+        }else{
+            return AVLtree.init()
+        }
+    }
+    
+    func rotate_RL() -> AVLtree
+    {
+        self.left = self.left.rotate_left();
+        return self.rotate_right()
+    }
+    
+    func rotate_LR() -> AVLtree
+    {
+        self.right = self.right.rotate_right();
+        return self.rotate_left()
+    }
+
+    
+
+}
--- helloSpriteKit/GameScene.swift
+++ helloSpriteKit/GameScene.swift
@@ -9,20 +9,125 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    //instance variables
+    let man = SKSpriteNode(imageNamed:"StickMan")
+    var tree:AVLtree = AVLtree.init()
+    let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+    var currentTreeLabels: [SKLabelNode] = []
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
+        
+        //label setup
+        myLabel.text = ""
+        myLabel.fontColor = UIColor.blackColor()
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
+        //sprite setup
+        man.anchorPoint = CGPointMake(0.5, 0)
+        //man.position = CGPointMake(self.frame.size.width/2, 0)
+        man.xScale = 0.12
+        man.yScale = 0.12
+        
+        //scene setup
+        self.backgroundColor = UIColor.whiteColor()
+        
+        self.insertToTree(5)
+        self.insertToTree(2)
+        //self.tree = tree.rotate_RL()
+        
+        man.position = CGPointMake(self.frame.size.width/2, self.getTreeYAtDepth(1.0) + 50)
+        
+        displayEntireTree()
+        
+        //add elements to self
         self.addChild(myLabel)
+        self.addChild(man)
+    }
+    
+    //tree displaying code
+    
+    func displayEntireTree()
+    {
+        self.removeChildrenInArray(currentTreeLabels)
+        for var i = 1; i <= tree.getHeight(); i += 1
+        {
+            self.displayTree(CGFloat(i))
+        }
+
+    }
+    
+    func displayTree(depth:CGFloat)
+    {
+        
+        print("in method")
+        let center_at = (self.frame.width/2.0)/(pow(2.0,depth-1.0)+1.0)
+        print ("Center_at = \(center_at)")
+        var current = center_at + 256.0
+        print ("Current = \(current)")
+        let arr = tree.getNumsAtDepth(Int(depth))
+        print("arr = \(arr)")
+        
+        for a in arr
+        {
+            print ("looping")
+            let newLabel = SKLabelNode(fontNamed: "Chalkduster")
+            
+            if (a == -1)
+            {
+                newLabel.text = ""
+            }else{
+                newLabel.text = String(a)
+            }
+            
+            newLabel.fontColor = UIColor.blackColor()
+            newLabel.fontSize = 30
+            newLabel.position = CGPoint(x: current, y: CGFloat(getTreeYAtDepth(depth)))
+            currentTreeLabels.append(newLabel)
+            self.addChild(newLabel)
+            current = current + center_at
+            print ("Current = \(current)")
+        }
+        
+    }
+    
+    //highest y coordinate is at depth 1
+    func getTreeYAtDepth(depth:CGFloat) -> CGFloat
+    {
+        return 100*(CGFloat(tree.getHeight())-depth)+50
+    }
+    
+    //updating code
+    
+    func insertToTree(ins:Int)
+    {
+            
+        //insert redisplay reposition
+        tree.insertIntNoRotation(ins)
+        self.displayEntireTree()
+        man.position = CGPointMake(self.frame.size.width/2, self.getTreeYAtDepth(1.0) + 50)
+        let balanceFactor = tree.balanceFactor()
+        if (balanceFactor == 1)
+        {
+            //left>right
+            man.zRotation = CGFloat(M_PI/4);
+        }else if (balanceFactor == -1)
+        {
+            //left>right
+            man.zRotation = CGFloat(-1*M_PI/4);
+        }else if (abs(balanceFactor) > 1)
+        {
+            myLabel.text = "Game over"
+        }
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
+        /*for touch in touches {
             let location = touch.locationInNode(self)
             
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
@@ -36,10 +141,12 @@ class GameScene: SKScene {
             sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
-        }
+        }*/
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        //man.position = CGPointMake(man.position.x, man.position.y+1)
+        
     }
 }
